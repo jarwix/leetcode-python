c67f0f12728be1c04fabf6a08538fd31cb28d506
jarwix <oazisrus@gmail.com>
2020-10-12 18:19:53 +0300

    Create Easy 1528 Shuffle String.py

---
+++ Easy 1528 Shuffle String.py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = ""
+        for i in range(len(s)):
+            result = result + s[indices.index(i)]
+        return result
